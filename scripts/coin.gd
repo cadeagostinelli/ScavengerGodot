e87ae8151bb818d578052ad49fd3f6cc25765875
Danielle <dw2360@msstate.edu>
2025-03-02 00:14:32 -0600

    I scaled down the coins, added audio for the coins, and created a folder for the audio files

--- scripts/coin.gd
+++ scripts/coin.gd
@@ -3,7 +3,12 @@ extends Node2D
 var Player = preload("res://scripts/player.gd")
 
 @export var value: int = 1
+@export var desired_scale: Vector2 = Vector2(1, 1) # for scaling the coins
 
+func _ready():
+	add_to_group("Coins")
+	self.scale = desired_scale # applies consistent scaling
+	
 func _on_area_2d_body_entered(body):
 	if body is Player:
 		GameController.coin_collected(value)
--- scripts/game_controller.gd
+++ scripts/game_controller.gd
@@ -2,7 +2,13 @@ extends Node
 
 
 var total_coins: int = 0
-
+func _ready():
+	var audio_player = AudioStreamPlayer.new()
+	add_child(audio_player)
+	audio_player.stream = preload("res://audio/coin.wav")
+	audio_player.name = "CoinAudio"
+	
 func coin_collected(value: int):
 	total_coins += value 
+	$CoinAudio.play()
 	EventController.emit_signal("coin_collected", total_coins)
