486a137b82638144c6fa1ea92672c17f1afa3667
Netclicks <evan.robertson.lane@icloud.com>
2025-03-16 12:48:43 -0500

    Saving my changes before pull

---
+++ scripts/player_spawner.gd
@@ -0,0 +1,11 @@
+## Spawner Script
+extends Node2D
+
+var player_scene = preload("res://player.tscn")
+var player = null
+func _process(_delta):
+	if player == null:
+		var new_obj = player_scene.instantiate()
+		new_obj.position = position
+		get_parent().add_child(new_obj)
+		player = new_obj
