3676d60bdbf5395681e003b21b73effb0a934ab3
Danielle <dw2360@msstate.edu>
2025-03-16 22:52:43 -0500

    Updated the title screen to have a background, same title image as slides from first presentation, and added custom start and quit button images

--- scripts/title_screen.gd
+++ scripts/title_screen.gd
@@ -1,9 +1,15 @@
 extends Control
 
+func _ready():
+	var title = $Title
+	# Resizes node to match screen size
+	var screensize = get_viewport().get_size()
+	var background = $Background
+	var start_button = $Start
+	background.custom_minimum_size = screensize
 
-func _on_start_button_pressed() -> void:
+func _on_start_pressed() -> void:
 	get_tree().change_scene_to_file("res://level1.tscn")
-
-
-func _on_quit_button_pressed() -> void:
+	
+func _on_quit_pressed() -> void:
 	get_tree().quit()
