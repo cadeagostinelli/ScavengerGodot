a5af9fb15cb9a806106cb5a68ad5927f1f2d0666
Danielle <dw2360@msstate.edu>
2025-03-17 09:55:49 -0500

    Redid the title screen and added a game over audio in case we want to use it.

--- scripts/title_screen.gd
+++ scripts/title_screen.gd
@@ -6,9 +6,15 @@ func _ready():
 	var screensize = get_viewport().get_size()
 	var background = $Background
 	var start_button = $Start
+	var title_player = AudioStreamPlayer.new()
+	add_child(title_player)
+	title_player.stream = preload("res://audio/Audio1.wav")
+	title_player.name = "TitleAudio"
+	$TitleAudio.play()
 	background.custom_minimum_size = screensize
 
 func _on_start_pressed() -> void:
+	$TitleAudio.stop()
 	get_tree().change_scene_to_file("res://level_1REAL.tscn")
 	
 func _on_quit_pressed() -> void:
